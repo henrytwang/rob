587e050abba5604c4244ff7517e11dbb4d6c0bb8
Henry Wang <henry.t.wang@gmail.com>
2013-06-10 21:08:13 -0500

    updated config file again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,9 +46,10 @@ RailsBirds::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
 
   config.assets.compile = true
+  config.assets.precompile += %w( *.js *.css )
+  config.serve_static_assets = true
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
